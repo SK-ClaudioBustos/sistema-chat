1427e8dc4f8e2d26d1f16f492ce5c64b3314aab7
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-10-30 19:21:22 -0300

    feat: separate icon from component

--- chat-frontend/src/components/ChatPage/ChatView/NoChatSelected.tsx
+++ chat-frontend/src/components/ChatPage/ChatView/NoChatSelected.tsx
@@ -1,27 +1,17 @@
+import { BigCommentIcon } from "@/icons/BigCommentIcon";
+
 export const NoChatSelected = () => {
   return (
     <div className="flex-1 flex items-center justify-center bg-gray-50 dark:bg-gray-900">
       <div className="text-center">
         <div className="mb-4">
-          <svg
-            className="mx-auto h-24 w-24 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1}
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-            />
-          </svg>
+          <BigCommentIcon />
         </div>
         <h3 className="text-xl font-semibold text-gray-900 dark:text-white mb-2">
           Ningún chat seleccionado
         </h3>
         <p className="text-gray-500 dark:text-gray-400">
-            Seleccione un usuario para empezar a chatear
+          Seleccione un usuario para empezar a chatear
         </p>
       </div>
     </div>
---
+++ chat-frontend/src/icons/BigCommentIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export const BigCommentIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      {...props}
+      className="mx-auto h-24 w-24 text-gray-400"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1}
+        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+      />
+    </svg>
+  );
+};
